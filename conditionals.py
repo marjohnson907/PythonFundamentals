6d60e1fad1c7508b9972a6dd89931b528c3f1fd5
marjohnson907 <112601108+marjohnson907@users.noreply.github.com>
2022-10-11 18:23:43 -0600

    updated guessing game with while loop

--- conditionals.py
+++ conditionals.py
@@ -2,29 +2,54 @@
 # Method evaluate data
 # if then else
 
+
 import random
 
-high_range = 100
-middle_number = int(high_range/2)
-my_guess = middle_number
-number_guesses = 0
-highOrLow = "lower"
-output = "{} is {} than {}"
+#ask the user to select the upper bound
+upper_bound = input("What is the upper bound? ")
+upper_bound = int(upper_bound)
+
+#generate a random integer starting at 1 and going to upper_bound
+random_number = random.randint(1,upper_bound)
+print(random_number)
+
+user_guess = None
+#start and loop here
+while user_guess != random_number:
+    #ask the user to guess
+    user_guess = input("Guess a number between 1 and " + str(upper_bound) + ": ")
+
+    #check if user guess = random number
+    if int(user_guess) == random_number: 
+        print("You win.")
+        #exit the loop
+    else: 
+        print("You lose.")
+print("Game Over")
 
 
-my_random_number = random.randint(1,high_range)
-
-print(my_random_number)
-print(my_guess)
-
-#dry: don't repeat yourself
-#refactor
-
+# high_range = 100
+# middle_number = int(high_range/2)
+# my_guess = middle_number
+# number_guesses = 0
+# highOrLow = "lower"
+# output = "{} is {} than {}"
 
 
-# evaluate the random number and compare it to the middle number
-if my_guess < my_random_number :
-    highOrLow = "lower"
-if my_guess > my_random_number :
-    highOrLow = "higher"
-print(output.format(my_guess,highOrLow,my_random_number))
+# my_random_number = random.randint(1,high_range)
+
+# print(my_random_number)
+# print(my_guess)
+
+# #dry: don't repeat yourself
+# #refactor
+
+
+
+# # evaluate the random number and compare it to the middle number
+# if my_guess < my_random_number :
+#     highOrLow = "lower"
+# if my_guess > my_random_number :
+#     highOrLow = "higher"
+# print(output.format(my_guess,highOrLow,my_random_number))
+
